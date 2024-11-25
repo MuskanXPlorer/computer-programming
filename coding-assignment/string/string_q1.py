3a5cf3de54320766c2566d3abc1a6f62dfdb8c95
MuskanXPlorer <th.utsavthakur@gmail.com>
2024-11-25 15:08:56 +0530

    pallindrome

--- coding-assignment/string/string_q1.py
+++ coding-assignment/string/string_q1.py
@@ -1,4 +1,10 @@
-'''reverse of a string'''
-str  = "hello world"
-out = str[::-1]
-print(out)
+'''Write aprogram to check a  string is a pallindrome.'''
+#input from user
+st = input("enter your string: ")
+#reverse of the string
+out = st[::-1]
+#pallin check
+if st==out:
+    print(True)
+else:
+    print(False)
