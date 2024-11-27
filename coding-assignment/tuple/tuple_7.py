5f5e4857326617eb24baa54fdf7c71a222e1ecf9
MuskanXPlorer <th.utsavthakur@gmail.com>
2024-11-27 14:18:06 +0530

    reverse of tuple

---
+++ coding-assignment/tuple/tuple_7.py
@@ -0,0 +1,9 @@
+'''Write a program reverse the elements of a tuple.'''
+# Creating a tuple
+my_tuple = (1, 2, 3, 4, 5)
+
+# Reversing the tuple
+reversed_tuple = my_tuple[::-1]
+
+# Printing the reversed tuple
+print("Reversed tuple:", reversed_tuple)
