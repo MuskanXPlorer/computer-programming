3ed706a531c67ef64cac0a19e0c919b4fce3601c
MuskanXPlorer <th.utsavthakur@gmail.com>
2024-11-27 18:13:45 +0530

    count vowels

---
+++ coding-assignment/function/function_q5.py
@@ -0,0 +1,8 @@
+'''Create a function that counts the number of vowels in a given string.'''
+def count_vowels(s):
+    vowels = 'aeiouAEIOU'
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
