d3d458be0f9618c5d2dba0525feb2cd2712a5433
MuskanXPlorer <th.utsavthakur@gmail.com>
2024-11-27 14:40:24 +0530

    add a new key-value pair

---
+++ coding-assignment/dict/dict_q3.py
@@ -0,0 +1,10 @@
+'''Write a program to add a new key-value pair to an existing dictionary.'''
+# Creating a dictionary
+my_dict = {'name': 'Alice', 'age': 25, 'city': 'New York'}
+
+# Adding a new key-value pair
+my_dict['country'] = 'USA'
+
+# Printing the updated dictionary
+print("Updated Dictionary:", my_dict)
+
