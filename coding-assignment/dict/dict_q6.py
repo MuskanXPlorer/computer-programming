c05d0266689db2d5753fd8a6c5aa133d235beb1a
MuskanXPlorer <th.utsavthakur@gmail.com>
2024-11-27 14:44:43 +0530

    dict comprehension

---
+++ coding-assignment/dict/dict_q6.py
@@ -0,0 +1,6 @@
+'''Write a  program that maps numbers to their squares.'''
+# Creating the dictionary
+squares_dict = {x: x**2 for x in range(1, 6)}
+
+# Printing the dictionary
+print("Squares Dictionary:", squares_dict)
