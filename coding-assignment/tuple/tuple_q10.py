02f2da59b30f582560e64f69f16af00344e712c4
MuskanXPlorer <th.utsavthakur@gmail.com>
2024-11-27 14:29:30 +0530

    tuple without parenthesis

---
+++ coding-assignment/tuple/tuple_q10.py
@@ -0,0 +1,6 @@
+'''Write a program to create a tuple without using parentheses.'''
+# Creating a tuple without parentheses
+no_parentheses_tuple = 1, 2, 3
+
+# Printing the tuple
+print("Tuple without parentheses:", no_parentheses_tuple)
