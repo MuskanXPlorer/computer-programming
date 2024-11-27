d093306e7023f2dff0f72eabb663494c8f3ec643
MuskanXPlorer <th.utsavthakur@gmail.com>
2024-11-27 14:24:07 +0530

    repeat a tuple multiple times

---
+++ coding-assignment/tuple/tuple_q8.py
@@ -0,0 +1,9 @@
+'''Write a program to repeat a tuple multiple times.'''
+# Creating a tuple
+repeat_tuple = ('repeat',)
+
+# Repeating the tuple 4 times
+repeated_tuple = repeat_tuple * 4
+
+# Printing the repeated tuple
+print("Repeated tuple:", repeated_tuple)
