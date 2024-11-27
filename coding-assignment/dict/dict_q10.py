1514800d32ae370421ea4c8b80ec983ac55bb623
MuskanXPlorer <th.utsavthakur@gmail.com>
2024-11-27 14:53:00 +0530

    sum of elements

---
+++ coding-assignment/dict/dict_q10.py
@@ -0,0 +1,9 @@
+'''Write a program sum all the values in a dictionary.'''
+# Creating a dictionary
+my_dict = {'a': 10, 'b': 25, 'c': 30, 'd': 15}
+
+# Summing values
+total_sum = sum(my_dict.values())
+
+# Printing the sum
+print("Sum of values:", total_sum)
