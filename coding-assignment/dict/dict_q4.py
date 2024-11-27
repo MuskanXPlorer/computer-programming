3e865214f80ce54259dfc738dd1570adfb2c8b50
MuskanXPlorer <th.utsavthakur@gmail.com>
2024-11-27 14:41:37 +0530

    remove a key-value pair

---
+++ coding-assignment/dict/dict_q4.py
@@ -0,0 +1,9 @@
+'''Write a program to remove a key-value pair from a dictionary.'''
+# Creating a dictionary
+my_dict = {'name': 'Alice', 'age': 25, 'city': 'New York'}
+
+# Removing a key-value pair
+del my_dict['city']
+
+# Printing the updated dictionary
+print("Updated Dictionary:", my_dict)
